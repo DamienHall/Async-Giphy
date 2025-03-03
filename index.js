12cd077614b36d5e62acac7c247f137d622b3531
Scheltema <thomas.scheltema@hp.com>
2025-03-03 11:28:23 -0600

    create getImage request function

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 require('dotenv').config();
 
 // Print out value of API key stored in .env file
-console.log(process.env.API_KEY)
+console.log(process.env.API_KEY);
+
+async function getImages(query) {
+  const endpoint = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en`);
+  const data = await endpoint.json();
+  const gifURL = data.data[0].url;
+  console.log(gifURL);
+}
+
+getImages("dogs");
